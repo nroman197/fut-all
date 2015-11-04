b03826dac4f1798f757f3724e2c1faae7ff6beb3
nroman197 <nroman197@gmail.com>
2015-11-04 01:55:46 -0600

    Test

---
+++ fut-all/fut-all/Add_Tourn_Cup.aspx.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace fut_all
+{
+    public partial class WebForm3 : System.Web.UI.Page
+    {
+        WS ws = new WS();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                LoadCountries();
+            }
+        }
+
+        protected void imgLogoHome_Click(object sender, ImageClickEventArgs e)
+        {
+
+        }
+
+        protected void ddlTeamType_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void ddlTeamCathegory_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LoadCountries()
+        {
+
+            List<string> theList = ws.Countries_Get();
+
+            System.Data.DataTable tb = new System.Data.DataTable();
+
+            // manage gridview
+            tb.Columns.Add("Name");
+
+
+            foreach (string g in theList)
+            {
+
+               tb.Rows.Add(g);
+
+            }
+
+            grvCountries.DataSource = tb;
+            grvCountries.DataBind();
+        }
+
+        protected void DropDownList1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnAddTeam_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
